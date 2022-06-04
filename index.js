b5da6a8893875928f5e5436c088f60ec4ba0a55e
Danang Adi Nugroho <danangtar@gmail.com>
2022-06-04 15:06:52 +0700

    add sorter function

--- index.js
+++ index.js
@@ -6,15 +6,25 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = function(arr, alg) {
+  var temp = [];
+  alg(arr).forEach((element, index) => {
+    temp.push(`${index + 1}. ${element}`);
+  });
+  return temp;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = function(arr) {
+  return arr.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = function(arr) {
+  return arr.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
